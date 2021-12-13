b5701df3b68b8ba63e6341b1477821ba37d1783b
Felix Geisendörfer <felix@felixge.de>
2021-12-13 14:34:56 +0100

    add div

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ func main() {
 	fmt.Println(Add(2, 3))
 	fmt.Println(Sub(3, 2))
 	fmt.Println(Mul(3, 2))
+	fmt.Println(Div(3, 2))
 }
 
 func Add(a, b int) int {
@@ -18,4 +19,8 @@ func Sub(a, b int) int {
 
 func Mul(a, b int) int {
 	return a * b
+}
+
+func Div(a, b int) int {
+	return a / b
 }
